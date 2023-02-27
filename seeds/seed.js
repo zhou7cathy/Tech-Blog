ddd58cd3d72ae541896f33ac7825b14547dbe4b1
unknown <zhou7cathy@gmail.com>
2023-02-27 10:35:37 +0800

    update seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Post, Comment } = require('../models');
 
-const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const userData = require('./userSeedData.json');
+const postData = require('./postSeedData.json');
+const commentData = require('./commentSeedData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,10 +13,22 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+
+  const posts = [];
+  for (const post of postData) {
+    posts.push(await Post.create({
+      ...post,
       user_id: users[Math.floor(Math.random() * users.length)].id,
+    }, {
+      returning: true
+    }));
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      post_id: posts[Math.floor(Math.random() * posts.length)].id,
     });
   }
 
